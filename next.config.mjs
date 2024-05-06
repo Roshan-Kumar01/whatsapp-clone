1a49d318554f13535cfec6b26b6742f1c2b5c2af
Roshan Kumar <roshankumarrtk81@gmail.com>
2024-05-06 17:55:43 +0530

    done with send images and videos

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        remotePatterns:[
+            {hostname:"dependable-ferret-649.convex.cloud"}
+        ]
+    }
+};
 
 export default nextConfig;
